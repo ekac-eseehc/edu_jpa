d6d46b6e95faafd6f8e632a51a79756cb8c0da05
ekac <eseehc080@gmail.com>
2023-02-13 22:45:58 +0900

    different types of association mapping
    many to many [ n : m ]

--- ex1_hello_jpa/src/main/java/hellojpa/Member.java
+++ ex1_hello_jpa/src/main/java/hellojpa/Member.java
@@ -1,10 +1,15 @@
 package hellojpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
@@ -25,6 +30,10 @@ public class Member {
 	@OneToOne
 	@JoinColumn(name = "LOCKER_ID")
 	private Locker locker;
+	
+	@ManyToMany
+	@JoinTable(name = "member")
+	private List<MemberProduct> memberProduct = new ArrayList<>();
 
 	public Long getId() {
 		return id;
