9a2cd7260f2a14b9f25fcbbe7ae5a8a2f985198b
ekac <eseehc080@gmail.com>
2023-02-26 15:13:03 +0900

    jpql 1
    jpql basic grammar and query API

---
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,39 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+
+public class JpaMain {
+	public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+        	
+        	Member member = new Member();
+        	member.setUsername("Kim");
+        	member.setAge(10);
+        	em.persist(member);
+        	
+        	Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+        			.setParameter("username", "Kim")
+        			.getSingleResult();
+        	System.out.println("result : " + result.getUsername());
+        	
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
