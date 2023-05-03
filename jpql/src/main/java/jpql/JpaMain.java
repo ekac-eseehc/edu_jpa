773b5f2e1d35fa01034f99c38a7768d0a4af5e67
js <eseehc080@gmail.com>
2023-05-03 13:45:07 +0900

    jpql 2
    fetch join 1

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,5 @@
 package jpql;
 
-import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -18,29 +17,46 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			Team team = new Team();
-			em.persist(team);
+			Team teamA = new Team();
+			teamA.setName("TeamA");
+			em.persist(teamA);
+			
+			Team teamB = new Team();
+			teamB.setName("TeamB");
+			em.persist(teamB);
 
-			Member member = new Member();
-			member.setUsername("admin");
-			member.setTeam(team);
-			em.persist(member);
+			Member member1 = new Member();
+			member1.setUsername("member1");
+			member1.setTeam(teamA);
+			em.persist(member1);
 
 			Member member2 = new Member();
-			member2.setUsername("admin2");
-			member2.setTeam(team);  
+			member2.setUsername("member2");
+			member2.setTeam(teamA);  
 			em.persist(member2);
+			
+			Member member3 = new Member();
+			member3.setUsername("member3");
+			member3.setTeam(teamB);  
+			em.persist(member3);
 			
 			em.flush();
 			em.clear();
 
-			String query = "select m.username From Team t join t.members m ";
+			String query = "select distinct t From Team t join fetch t.members ";
 
-			List<Collection> result = em.createQuery(query, Collection.class)
+			List<Team> result = em.createQuery(query, Team.class)
 					.getResultList();
+			
+			System.out.println("result : " + result.size());
 
-			System.out.println("str = " + result);
-
+			for (Team team : result) {
+				System.out.println("member : " + team.getName() + ", " + team.getMembers().size());
+				for (Member member : team.getMembers()) {
+					System.out.println(" -> member : " + member);
+				}
+			}
+			
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
